5e67ec5ded8a785898a4849167c70d986dc8d7a0
Joao Mota <joao.mota@tekoa.com.br>
2017-10-06 17:04:34 -0300

    Security code input

--- src/App.js
+++ src/App.js
@@ -5,20 +5,50 @@
  */
 
 import React from 'react';
-import { View, Styles, Button, Text, Component } from 'reactxp';
+import { View, Styles, Button, Text, Component, TextInput } from 'reactxp';
 
 export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        securityCode: '',
+        canSearch: false
+    };
+  }
+
+  // http://rest.milangladis.com/
+  // https://codepen.io/anon/pen/mepogj?editors=001
+  // https://github.com/JedWatson/classnames
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
+        <div style={styles.codeRadioContainer}>
+          <TextInput 
+            placeholder="Enter your security code here..." 
+            style={styles.codeRadioSecurityCode} 
+            maxLength="4" 
+            onChangeText={ this._onChangeSecurityCode }
+            autoFocus={true}
+          />
+          <div style={styles.codeRadioSecuritySubmit}>
+            <div style={styles.codeRadioSecurityInputHelper}>press [ENTER] or</div>
+            Search
+          </div>
+        </div>
       </View>
     );
+  }
+
+  _onChangeSecurityCode = (newValue) => {
+    if(newValue.length === 4) {
+      this.setState({securityCode: newValue});
+      this.setState({canSearch: true});
+    } else {
+      this.setState({securityCode: ''});
+      this.setState({canSearch: false});
+    }
   }
 }
 
@@ -27,16 +57,62 @@ const styles = {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fbfbfb',
   }),
-  welcome: Styles.createTextStyle({
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  codeRadioContainer: Styles.createWebViewStyle({
+    position: 'relative',
+    zIndex: 1,
+    background: '#fff',
+    display: 'block',
+    borderRadius: '4px',
+    width: '100%',
+    maxWidth: '700px',
+    margin: 'auto'
   }),
-  instructions: Styles.createTextStyle({
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  codeRadioSecurityCode: Styles.createTextInputStyle({
+    backgroundColor: 'transparent',
+    boxShadow: '0px 5px 30px rgba(0, 0, 0, 0.1)',    
+    color: '#686767',
+    border: 'none',
+    font: 'inherit',
+    fontSize: '14px',
+    height: '70px',
+    lineHeight: '70px',
+    marginLeft: 0,
+    outline: 'none',
+    overflow: 'hidden',
+    paddingLeft: '30px',
+    paddingRight: '175px',
+    transition: 'all 0.3s ease',
+    width: '100%',
+    whiteSpace: 'nowrap',
   }),
+  codeRadioSecuritySubmit: Styles.createViewStyle({
+    position: 'absolute',
+    height: '50px',
+    padding: '0 30px',
+    top: '10px',
+    right: '10px',
+    color: '#fff',
+    fontSize: '12px',
+    letterSpacing: '0.5px',
+    lineHeight: 50,
+    textTransform: 'uppercase',
+    background: '#EDEDEF',
+    borderRadius: '2px',
+    cursor: 'pointer',
+    transition: 'all 0.3s ease',
+    pointerEvents: 'none',
+  }),
+  codeRadioSecurityInputHelper: Styles.createWebViewStyle({
+    opacity: 0,
+    position: 'absolute',
+    right: '100%',
+    color: '#A17BBC',
+    whiteSpace: 'nowrap',
+    fontSize: '10px',
+    marginRight: '5px',
+    pointerEvents: 'none',
+    transition: 'all 0.3s ease',
+  })
 };
